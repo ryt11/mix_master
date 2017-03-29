1759a7288af776304f53242b88ea56a364035c7e
ryt11 <rileyt_2000@hotmail.com>
2017-03-29 15:30:16 -0600

    add iteration 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :artists# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists do
+   resources :songs, only: [:new, :create]
+ end
+ resources :songs, only: [:show]# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
